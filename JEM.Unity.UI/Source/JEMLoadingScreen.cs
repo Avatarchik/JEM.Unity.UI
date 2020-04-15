118a0937ade83ac4fe63c7b07b6738e78493716f
Adam Majcherek <dem0n1358v2@gmail.com>
2020-04-15 22:53:57 +0200

    JEMLoadingScreen component.

---
+++ JEM.Unity.UI/Source/JEMLoadingScreen.cs
@@ -0,0 +1,210 @@
+﻿//
+// JEM For Unity
+//
+// Copyright (c) 2020 ADAM MAJCHEREK ALL RIGHTS RESERVED
+//
+
+using System;
+using System.Collections;
+using JEM.Unity.Attribute.Behaviour;
+using JEM.Unity.Attribute.Style;
+using JEM.Unity.UI.Animation;
+using UnityEngine;
+
+namespace JEM.Unity.UI
+{
+    /// <summary>
+    ///     A universal component that may be used as scenario for your loading screen.
+    /// </summary>
+    /// <remarks>
+    ///    IMPORTANT: This component does not implements any code that controls <see cref="Time.timeScale"/>.
+    ///            You need to implement your own Time.TimeScale management while game is waiting for user to continue.
+    /// </remarks>
+    public sealed class JEMLoadingScreen : JEMSingletonBehaviour<JEMLoadingScreen>
+    {
+        /// <summary>
+        ///     Root panel of the loading UI.
+        /// </summary>
+        [Header("Settings")]
+        [JEMInfo(JEMInfoCondition.NegativeValue, "Value need to be set.", Type = JEMInfoMessageType.Error)]
+        public JEMInterfaceFadeAnimation LoadingPanel;
+
+        /// <summary>
+        ///     Defines whether an additional loading animation is being drawn to user.
+        /// </summary>
+        public bool HasLoadingAnimation;
+
+        /// <summary>
+        ///     Reference to the root of loading animation.
+        /// </summary>
+        [JEMSpace]
+        [JEMIndent, JEMShowIf(nameof(HasLoadingAnimation))]
+        [JEMInfo(JEMInfoCondition.NegativeValue, "Value need to be set.", Type = JEMInfoMessageType.Error)]
+        public JEMInterfaceFadeAnimation LoadingAnimationPanel;
+
+        /// <summary>
+        ///     Time in seconds of how much loading animation should wait to fadeout after loading ends.
+        ///     If you have <see cref="WaitForUserContinue"/> root loading panel will wait for user input to get disabled.
+        /// </summary>
+        [JEMIndent, JEMShowIf(nameof(HasLoadingAnimation))]
+        public float LoadingAnimationFadeout = 2f;
+        
+        /// <summary>
+        ///     While true, loading screen will continue only if user continues.
+        ///     NOTE: You need to call <see cref="ContinueByUser"/> method to continue if <see cref="CanContinueByAnyKey"/> is not enabled.
+        /// </summary>
+        [JEMIndent, JEMShowIf(nameof(HasLoadingAnimation))]
+        public bool WaitForUserContinue = true;
+
+        /// <summary>
+        ///     Reference to panel that could tell that input is needed in order to continue.
+        /// </summary>
+        [JEMSpace]
+        [JEMIndent(2), JEMShowIf(nameof(WaitForUserContinue))]
+        [JEMInfo(JEMInfoCondition.NegativeValue, "Value need to be set.", Type = JEMInfoMessageType.Error)]
+        public JEMInterfaceFadeAnimation ContinueInfoPanel;
+        
+        /// <summary>
+        ///     When enabled, loading screen will continue by entering any key.
+        /// </summary>
+        [JEMIndent(2), JEMShowIf(nameof(WaitForUserContinue))]
+        public bool CanContinueByAnyKey = true;
+
+        /// <summary>
+        ///     Called when user wants to continue after loading ends.
+        /// </summary>
+        public event Action OnUserContinue;
+
+        /// <summary>
+        ///     Called at the beginning of <see cref="ReportLoadingState"/> call/When loading state is reported to UI.
+        /// </summary>
+        public event Action<bool> OnLoadingStateReported;
+        
+        /// <summary>
+        ///     Defines whether the loading UI is active or not.
+        /// </summary>
+        public bool IsLoadingActive { get; private set; }
+        
+        /// <summary>
+        ///     True when UI is waiting for user input to continue.
+        /// </summary>
+        public bool IsWaitingForUser { get; private set; }
+        
+        private bool _dontWaitForContinue;
+        private bool _loadingFadeOut;
+        
+        /// <inheritdoc />
+        protected override void OnAwake()
+        {
+            if (LoadingPanel == null) throw new NullReferenceException("LoadingPanel property is not set to an instance of object.");
+            
+            // make sure that loading panel is disabled
+            LoadingPanel.SetActive(false, true);
+            IsLoadingActive = false;
+        }
+
+        private void LateUpdate()
+        {
+            if (!CanContinueByAnyKey || !IsWaitingForUser) return;
+            if (Input.anyKeyDown)
+            {
+                ContinueByUser();
+            }
+        }
+        
+        /// <summary>
+        ///     Activates loading UI.
+        /// </summary>
+        /// <param name="dontWaitForContinue">When true, loading UI will be forced to ignore 'WaitForUserContinue' feature.</param>
+        public void ActiveLoading(bool dontWaitForContinue = false)
+        {
+            if (IsLoadingActive) return;
+            IsLoadingActive = true;
+            IsWaitingForUser = false;
+            
+            _dontWaitForContinue = dontWaitForContinue;
+            _loadingFadeOut = false;
+            
+            LoadingPanel.SetActive(true);
+            
+            // Reset state.
+            if (HasLoadingAnimation) LoadingAnimationPanel.SetActive(true, true);
+            if (ContinueInfoPanel != null)
+                ContinueInfoPanel.SetActive(false, true);
+            
+            OnLoadingStateReported?.Invoke(true);
+        }
+
+        /// <summary>
+        ///     Try to disable loading UI.
+        ///     If active scenario does not need user input UI will be disabled immediately.
+        /// </summary>
+        /// <param name="dontWaitForContinue">
+        ///     When true, loading UI will be forced to ignore 'WaitForUserContinue' feature.
+        ///     Note: that if <see cref="ActiveLoading"/> has been called with dontWaitForContinue true, this value basically don't matter.
+        /// </param>
+        public void TryDisableLoading(bool dontWaitForContinue = false)
+        {
+            if (!IsLoadingActive) return;
+            if (IsWaitingForUser) return;
+            if (_loadingFadeOut) return;
+            
+            // Disable loading animation.
+            if (HasLoadingAnimation)
+                LoadingAnimationPanel.SetActive(false);
+            
+            // Try to wait for fadeout.
+            _loadingFadeOut = true;
+            StartCoroutine(FadeOutWorker(dontWaitForContinue));
+            
+            OnLoadingStateReported?.Invoke(false);
+        }
+
+        private IEnumerator FadeOutWorker(bool dontWaitForContinue)
+        {
+            yield return new WaitForSeconds(LoadingAnimationFadeout);
+            _loadingFadeOut = false;
+            
+            var shouldWaitForUser = HasLoadingAnimation && WaitForUserContinue && !dontWaitForContinue && !_dontWaitForContinue;
+            if (shouldWaitForUser)
+            {
+                // Wait for user input.
+                IsWaitingForUser = true;
+                if (ContinueInfoPanel != null)
+                    ContinueInfoPanel.SetActive(true);
+                
+                yield break;
+            }
+
+            DisableLoading();
+        }
+
+        private void DisableLoading()
+        {
+            if (!IsLoadingActive) return;
+            IsLoadingActive = false;
+            IsWaitingForUser = false;
+            
+            LoadingPanel.SetActive(false);
+        }
+        
+        /// <summary>
+        ///     Reports loading state to UI.
+        /// </summary>
+        public void ReportLoadingState(bool isLoading)
+        {
+            OnLoadingStateReported?.Invoke(isLoading);            
+        }
+        
+        /// <summary>
+        ///     Reports that user wants to continue.
+        /// </summary>
+        public void ContinueByUser()
+        {
+            // By continuing we just disabling panel.
+            DisableLoading();
+
+            OnUserContinue?.Invoke();
+        }
+    }
+}
---
+++ JEM.Unity.UITest/Source/JEMLoadingScreenTest.cs
@@ -0,0 +1,44 @@
+﻿//
+// JEM For Unity
+//
+// Copyright (c) 2020 ADAM MAJCHEREK ALL RIGHTS RESERVED
+//
+
+using System.Collections;
+using JEM.Unity.UI;
+using UnityEngine;
+
+namespace JEM.Unity.UITest
+{
+    public sealed class JEMLoadingScreenTest : MonoBehaviour
+    {
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(1f);
+            
+            JEMLoadingScreen.Instance.OnUserContinue += () =>
+            {
+                Debug.Log("JEMLoadingScenario received user input.");
+            };
+            
+            JEMLoadingScreen.Instance.OnLoadingStateReported += state =>
+            {
+                if (!state)
+                    Debug.Log("Loading UI is now disabled so we can initialize something new?"); // tho you should initialize your initialize during actual loading.
+                else
+                {
+                    Debug.Log("Loading enabled.");
+                }
+            };
+            
+            // Activate loading UI.
+            JEMLoadingScreen.Instance.ActiveLoading();
+            
+            // Wait 3 seconds just as we loading something.
+            yield return new WaitForSeconds(3f);
+            
+            // Report loading end.
+            JEMLoadingScreen.Instance.TryDisableLoading();
+        }
+    }
+}
